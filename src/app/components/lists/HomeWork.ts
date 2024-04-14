fde5a420161ad3a38032ccf0d1d978fefe595ab5
Felipe de Araujo <me@felipedearaujo.com>
2024-04-14 13:08:49 -0400

    Update experts profile picture

--- src/app/components/lists/HomeWork.ts
+++ src/app/components/lists/HomeWork.ts
@@ -1,46 +1,46 @@
 export interface HomeWorkListType {
-    title:string
-    img0:string
-    text0:string
-    img1:string
-    text1:string
-    img2:string
-    text2:string
-    img3:string
-    text3:string
+  title: string
+  img0: string
+  text0: string
+  img1: string
+  text1: string
+  img2: string
+  text2: string
+  img3: string
+  text3: string
 }
-export const HomeWorkList : HomeWorkListType[] = [
-    {
-        title:'500+ AI Experts',
-        img0:'/professional-1.webp',
-        text0:'LEGAL',
-        img1: '/professional-2.webp',
-        text1:'RISK',
-        img2:'/professional-3.webp',
-        text2:'FINANCE',
-        img3:'/professional-4.webp',
-        text3: 'MATERIALS',
-    },
-    {
-        title:'1000+ Integrations',
-        img0:'https://static.grammarly.com/assets/files/a0fad42a40ad6582b8ec0b0ad3503c34/google_docs.svg',
-        text0:'GOOGLE DOCS',
-        img1: 'https://static.grammarly.com/assets/files/9708475a150341fdf7c4af4a19a9c5e2/microsoft_word.svg',
-        text1:'WORD',
-        img2:'https://static.grammarly.com/assets/files/0352ceaa7ad5ecd806e3bda75a5edcb4/salesforce.svg',
-        text2:'SALESFORCE',
-        img3:'https://static.grammarly.com/assets/files/607a1fdaa43e2514310416705e172efc/notion.svg',
-        text3: 'NATION',
-    },
-    {
-        title:'Support All Branches',
-        img0:'/branch-army.png',
-        text0:'Army',
-        img1: 'branch-marinecorps.png',
-        text1:'Marine Corps',
-        img2:'branch-airforce.png',
-        text2:'Air Force',
-        img3:'branch-navy.png',
-        text3: 'Navy',
-    }
+export const HomeWorkList: HomeWorkListType[] = [
+  {
+    title: '500+ AI Experts',
+    img0: '/professional-1.png',
+    text0: 'LEGAL',
+    img1: '/professional-2.png',
+    text1: 'RISK',
+    img2: '/professional-3.png',
+    text2: 'FINANCE',
+    img3: '/professional-4.png',
+    text3: 'MATERIALS',
+  },
+  {
+    title: '1000+ Integrations',
+    img0: 'https://static.grammarly.com/assets/files/a0fad42a40ad6582b8ec0b0ad3503c34/google_docs.svg',
+    text0: 'GOOGLE DOCS',
+    img1: 'https://static.grammarly.com/assets/files/9708475a150341fdf7c4af4a19a9c5e2/microsoft_word.svg',
+    text1: 'WORD',
+    img2: 'https://static.grammarly.com/assets/files/0352ceaa7ad5ecd806e3bda75a5edcb4/salesforce.svg',
+    text2: 'SALESFORCE',
+    img3: 'https://static.grammarly.com/assets/files/607a1fdaa43e2514310416705e172efc/notion.svg',
+    text3: 'NATION',
+  },
+  {
+    title: 'Support All Branches',
+    img0: '/branch-army.png',
+    text0: 'Army',
+    img1: 'branch-marinecorps.png',
+    text1: 'Marine Corps',
+    img2: 'branch-airforce.png',
+    text2: 'Air Force',
+    img3: 'branch-navy.png',
+    text3: 'Navy',
+  },
 ]
